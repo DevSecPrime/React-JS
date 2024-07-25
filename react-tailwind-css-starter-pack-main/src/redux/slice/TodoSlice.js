793f2fceb3d6e0e8e90c665cb4383bc7060ed67a
Aryan R. Rana <110112868+DevSecPrime@users.noreply.github.com>
2024-07-25 12:17:06 +0530

    Add react-starter-pack-with-tailwind

---
+++ react-tailwind-css-starter-pack-main/src/redux/slice/TodoSlice.js
@@ -0,0 +1,35 @@
+
+import { createSlice, nanoid } from "@reduxjs/toolkit"
+
+// pass empty array
+const initialState = {
+    todos: [
+        // {
+        //     id: 1,
+        //     text: "hello word",
+        // }
+    ]
+}
+
+export const TodoSlice = createSlice({
+    name: "todo",
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+
+            state.todos.push(todo);
+        },
+
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        }
+    }
+
+})
+
+export const { addTodo, removeTodo } = TodoSlice.actions;
+export default TodoSlice.reducer;
---
+++ react-tailwind-css-starter-pack-main/src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import TodoSlice from "./slice/TodoSlice";
+
+export const store = configureStore({
+    reducer: {
+        todo: TodoSlice
+    }
+})
+
